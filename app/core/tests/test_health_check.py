48ab9c458365320b313658b2d2770a41323ce3aa
andrea.sasdelli <andrea.sasdelli@thinkopen.it>
2023-02-17 11:09:48 +0100

    lint fix

--- app/core/tests/test_health_check.py
+++ app/core/tests/test_health_check.py
@@ -4,8 +4,8 @@ Tests for the health check API.
 from django.test import TestCase
 from django.urls import reverse
 
-from rest_framework  import status
-from rest_framework.test  import APIClient
+from rest_framework import status
+from rest_framework.test import APIClient
 
 
 class HealthCheckTests(TestCase):
